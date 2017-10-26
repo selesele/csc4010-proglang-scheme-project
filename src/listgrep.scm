163863c18cada46efa2def88adca4cd999205e49
Caleb Currie <currie.j.caleb@gmail.com>
2017-10-25 20:34:21 -0500

    Implemented sublist and moved into one file.

---
+++ src/listgrep.scm
@@ -0,0 +1,13 @@
+; Takes the first num elements of the given list.
+(define (take xs num)
+  (cond ((zero? num) '()) ; If we've taken our amount, stop.
+        ((null? xs) '()) ; Similar case if we've run out of things to take.
+        (else (cons (car xs) (take (cdr xs) (- num 1)))))) ; Otherwise, recurse.
+
+; FIXME does not handle null lists properly. Issue appears to be with take.
+; Checks if the second list provided is a sublist of the first,
+(define (sublist? list1 list2)
+  (cond ((null? list1) (null? list2)) ; Handle the nulls.
+        ((equal? (take list1 (length list2)) list2) #t) ; Compare the first elements.
+        (else (sublist? (cdr list1) list2)))) ; Otherwise we recurse on the rest.
+
