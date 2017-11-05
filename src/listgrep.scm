1c4ab3dec8eb0a27d5882399e2e286600dcca009
Caleb Currie <currie.j.caleb@gmail.com>
2017-11-05 16:23:21 -0600

    Finished initial lgrep

--- src/listgrep.scm
+++ src/listgrep.scm
@@ -1,14 +1,29 @@
-; Takes the first num elements of the given list.
-; Returns the empty list if
-(define (take xs num)
-  (cond ((zero? num) '()) ; If we've taken our amount, stop.
-        ((null? xs) '()) ; Similar case if we've run out of things to take.
-        (else (cons (car xs) (take (cdr xs) (- num 1)))))) ; Otherwise, recurse.
-
-; FIXME does not handle null lists properly. Issue appears to be with take.
-; Checks if the second list provided is a sublist of the first,
+; TODO comments
 (define (sublist? list1 list2)
-  (cond ((null? list1) (null? list2)) ; Handle the nulls.
-        ((equal? (take list1 (length list2)) list2) #t) ; Compare the first elements.
-        (else (sublist? (cdr list1) list2)))) ; Otherwise we recurse on the rest.
+  (cond ((null? list1) #f) ; Initial list checks
+        ((null? list2) #f)
+        ((sublist-prime list1 list2) #t) ; Our helper function confirmed a sublist exists.
+        (else (sublist? list1 (cdr list2)))))
 
+; TODO comments
+(define (sublist-prime list1 list2)
+  (cond ((null? list1) #t) ; We have confirmed the full length of list1 inside list2.
+        ((null? list2) #f) ; We have exhausted list2.
+        ((equal? (car list1) (car list2))
+         (sublist-prime (cdr list1) (cdr list2))) ; We continue comparing.
+        (else #f))) ; We have a problem.
+
+
+; TODO comments
+(define (lgrep list1 list2)
+  (cond ((null? list1) '())
+        ((null? list2) '())
+        (else (lgrep-prime list1 list2 ))))
+
+; TODO comments
+(define (lgrep-prime list1 list2)
+  (cond ((null? list2) '())
+        ((sublist? list1 (car list2)) (cons (car list2) (lgrep-prime list1 (cdr list2))))
+        (else (lgrep-prime list1 (cdr list2)))))
+
+; TODO add tests, cover each case
