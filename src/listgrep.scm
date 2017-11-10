ce3ae88c0787cfefdb0f8ee7ea6003a70813c3e6
Caleb Currie <currie.j.caleb@gmail.com>
2017-11-10 16:59:38 -0600

    Updated testing strings for listgrep

--- src/listgrep.scm
+++ src/listgrep.scm
@@ -37,15 +37,46 @@
         ((sublist? list1 (car list2)) (cons (car list2) (lgrep-prime list1 (cdr list2)))) ; Use sublist? to check if list1 is in the current sublist of list2.  If so, recurse
         (else (lgrep-prime list1 (cdr list2))))) ; Otherwise, we still need to keep checking the rest of list2 recursively
 
-;; Testing for sublist?
 
+"Testing for sublist?"
 (sublist? '(1 2 3) '(4 5 9 2 1 2 1 2 3 2 1 3 2 1 2 3 5 6)) ; Should return true
+"should be"
+#t
+
+(newline)
+
 (sublist? '(5 4 3) '(2 5 5 4 3 6 7 5 4 3)) ; Should return true
+"should be"
+#t
+
+(newline)
+
 (sublist? '(2 2) '(6 8 1 4 8)) ; Should return false
+"should be"
+#f
+
+(newline)
+
 (sublist? '(1 2) '(a 4 6 * 5 3 g b 1 2 s)) ; Should return true
+"should be"
+#t
 
-;; Testing for lgrep
+(newline)
 
+
+"Testing for lgrep"
 (lgrep '(c d e) '((a b c d e f g)(c d c d e)(a b c d)(h i c d e k)(x y z))) ; Should return a lists containing lists 1, 2 and 4
+"should be"
+'((a b c d e f g) (c d c d e) (h i c d e k))
+
+(newline)
+
 (lgrep '(l i s t) '((l i l i s i)(1 2 l i 3 4)(5 6 l i s l i s t)(0 l i s t t))) ; Should return a list containing lists 3 and 4
+"should be"
+'((5 6 l i s l i s t) (0 l i s t t))
+
+(newline)
+
 (lgrep '(n 0) '((a b c)(1 2 3)(0 n 1 2)(3 2 1))) ; Should return an empty list
+"should be"
+'()
