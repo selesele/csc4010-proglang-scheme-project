3c344ca6861f4eeabb395687b2cc33447830336a
Will Nesbitt <inventingkid@gmail.com>
2017-11-13 23:26:42 -0600

    Add LL1 parser tests

---
+++ src/ll1-parser-test.scm
@@ -0,0 +1,27 @@
+#lang racket
+(load "ll1-parser.scm")
+"Testing for EPS"
+(newline)
+(eps calc-gram)
+"should be"
+'("FT" "SL" "TT")
+(newline)
+"Testing for FIRST"
+(newline)
+(gen-first-set "T" calc-gram (eps calc-gram))
+"should be"
+'("id" "num" "(")
+(newline)
+(gen-first-set "FT" calc-gram (eps calc-gram))
+"should be"
+'("*" "/")
+(newline)
+"Testing for FOLLOW"
+(newline)
+(gen-follow-set "T" calc-gram)
+"should be"
+'("+" "-" ")" "id" "read" "write" "$$")
+(newline)
+(gen-follow-set "FT" calc-gram)
+"should be"
+'("+" "-" ")" "id" "read" "write" "$$")
